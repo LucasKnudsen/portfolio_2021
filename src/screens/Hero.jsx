adf4a579bed8c76ccf4239a1743618e2c71a0458
lucas <laksteelhouse@gmail.com>
2021-05-01 20:06:33 +0200

    adds bubbles :)

--- src/screens/Hero.jsx
+++ src/screens/Hero.jsx
@@ -8,10 +8,35 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 
 const Hero = (props) => {
 
+  const bubbles = () => {
+    let bubbles = []
+    let count = 100
+    let i = 0
+    while (i < count) {
+
+      let x = Math.floor(Math.random() * window.innerWidth)
+      let y = Math.floor(Math.random() * window.innerHeight)
+      let size = Math.random()*8
+
+      bubbles.push(<motion.i animate={{opacity: [0,1,1,0], y: [0, -200]}} transition={{delay: 1+size, repeat: Infinity, duration: 10+size, times: [0,0.1,0.9,1]}}
+        style={{ left: x, top: y, width: 1+size, height: 1+size, 
+        background: i % 2 === 0 ? 'aquamarine' : 'transparent',
+        border: i % 2 === 0 ? 'none' : '1px solid aquamarine',
+        ...styles.bubbles
+      }}
+      ></motion.i>)
+      i++
+    }
+    return bubbles
+  }
+
   return (
     <motion.div style={styles.heroContainer}
       variants={heroFadeInVariants} initial='initial' animate='animate'
     >
+      <div>
+      {bubbles()}
+      </div>
       <motion.img src={profilePicture} alt="me!" style={styles.heroImage}
         initial='initial' animate='animate'
       />
@@ -65,7 +90,8 @@ const styles = {
     marginBottom: 45,
     borderRadius: '50%',
     height: '30vh',
-    width: '30vh'
+    width: '30vh',
+    zIndex: 1
   },
   heroButton: {
     display: 'flex',
@@ -80,4 +106,8 @@ const styles = {
     fontSize: 20,
     marginRight: 15,
   },
+  bubbles: {
+    position: 'absolute',
+    borderRadius: '100%',
+  }
 }
